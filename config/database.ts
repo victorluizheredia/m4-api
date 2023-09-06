e86c9f2e8236494ab04c2fe1e7b8fcafb05929d8
Victor luiz <victorluizheredia@gmail.com>
2023-09-06 16:05:48 -0300

    [config] ssl

--- config/database.ts
+++ config/database.ts
@@ -41,6 +41,9 @@ const databaseConfig: DatabaseConfig = {
         user: Env.get('PG_USER'),
         password: Env.get('PG_PASSWORD', ''),
         database: Env.get('PG_DB_NAME'),
+        ssl: Env.get('DB_SSL') ? {
+          rejectUnauthorized: false,
+        } : undefined,
       },
       migrations: {
         naturalSort: true,
